47c03589fb47cedc19815c22816a16665c2f0bd7
bacchantes <d.garciaherreros@icloud.com>
2020-11-23 03:36:07 +0100

    Add empty catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bacchus/git/project_course_es/src
+# Build directory: /home/bacchus/git/project_course_es/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
